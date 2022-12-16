d22943df70f95d45b48d7823071074fbc26022f3
Michael Wilmes <mwilmes@avc.edu>
2022-12-16 09:16:02 -0800

    Added helper script to update maxmind db Changed the name of the app in Laravel

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Polr'),
 
     /*
     |--------------------------------------------------------------------------
@@ -194,6 +194,11 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        /*
+         * GeoIP
+         */
+        \Torann\GeoIP\GeoIPServiceProvider::class,
 
     ],
 
