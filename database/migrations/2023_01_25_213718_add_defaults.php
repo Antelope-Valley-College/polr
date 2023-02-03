fe4c58e9ca947fa0a247a001e9aa493089de1f33
Michael Wilmes <mwilmes@avc.edu>
2023-02-03 10:27:56 -0800

    set default for creator

---
+++ database/migrations/2023_01_25_213718_add_defaults.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('links', function (Blueprint $table)
+        {
+            $table->string('creator')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('links', function (Blueprint $table)
+        {
+            $table->string('creator')->change();
+        });
+        
+    }
+};
