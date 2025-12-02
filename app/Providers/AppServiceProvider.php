0752e3200a160fa7e98e0c9192bcd41787c28e15
Michael Wilmes <michael.wilmes@avc.edu>
2025-12-02 09:22:03 -0800

    Force HTTPS if used as APP_PROTOCOL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // If the protocol is set to HTTPS, force HTTPS scheme for URLs
+        if (env('APP_PROTOCOL') === 'https://') {
+            \URL::forceScheme('https');
+        }
     }
 }
